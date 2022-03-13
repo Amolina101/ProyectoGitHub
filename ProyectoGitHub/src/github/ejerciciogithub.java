c2863465d5b366f77c607cd2a132215d596ce067
tano_ <tano_@DESKTOP-NS3QFTL>
2022-03-13 13:23:41 +0100

    added ejerciciogithub.java

--- ProyectoGitHub/src/github/ejerciciogithub.java
+++ ProyectoGitHub/src/github/ejerciciogithub.java
@@ -3,7 +3,7 @@ package github;
 public class ejerciciogithub {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("En este proyecto trabajamos con el repositorio GITHUB");
 
 	}
 
